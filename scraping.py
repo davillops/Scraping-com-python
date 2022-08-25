df35aaccbc73b79343cfa75c2d6d32957014f9f4
Davi Lopes <davidemedeiros86@gmail.com>
2022-08-25 20:32:54 -0300

    tratamento dos dados

--- scraping.py
+++ scraping.py
@@ -1,14 +1,27 @@
-from cgitb import html
-from urllib import response
 from urllib.request import urlopen
-from bs4 import BeautifulSoup
 
-url = 'https://alura-site-scraping.herokuapp.com/hello-world.php'
+url = 'https://alura-site-scraping.herokuapp.com/index.php'
 
 response = urlopen(url)
-html =  response.read()
+html = response.read()
+html
 
-soup = BeautifulSoup(html, 'html.parser')
+type(html) # Verificando o tipo de dado
+html = html.decode('utf-8') # Decodificando o codigo HTML para uma facil vizualização
+type(html) # Verificando novamente e dessa vez uma string ao inves de bytes
+html
 
-print(soup.find('h1', id="hello-world").get_text())
-print(soup.find('p').get_text())
+# Eliminando os caracteres de tabulação, quebra de linha etc
+
+html.split() # Transforma em um tipo de lista (onde tem espaços, ele quebra a linha), (array)
+
+" ".join(html.split()) # JOIN adiciona entre os caracteres
+
+" ".join(html.split()).replace('> <', '><') # REPLACE substitui caractere 'X' por 'Y'
+
+def trata_html(input): # fazendo o tratamento do HTML
+    return " ".join(input.split()).replace('> <', '><')
+
+html = trata_html(html) # atribuindo o tratamento
+html
+html
--- usando-headers.py
+++ usando-headers.py
@@ -1,3 +1,4 @@
+from cgitb import html
 from email import header
 from urllib import response
 from urllib.request import Request, urlopen
@@ -11,9 +12,10 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 try: # irá tentar executar o programa
     req = Request(url, headers= headers)
     response = urlopen(req)
-    print(response.read())
+    html = response.read()
+    print(html)
 
 except HTTPError as e: # caso a parte de cima de erro ira printar na tela o codigo do erro
     print(e.status, e.reason)
 except URLError as e: # URLerror não mostra os status
-    print(e.reason)   # Necessario ser nesta ordem para mostrar os erros de acordo
+    print(e.reason)   # Necessario ser nesta ordem para mostrar os erros de acord
