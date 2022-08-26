e477b0b12ebc15cd4f94c3f32e1c5cb147562c23
Davi Lopes <davidemedeiros86@gmail.com>
2022-08-26 15:40:27 -0300

    coletando mais dados

--- pratica.py
+++ pratica.py
@@ -9,12 +9,31 @@ sopinha = BeautifulSoup(codig, 'html.parser')
 
 cards = []
 card= {}
-
+#
 anuncio = sopinha.find('div', {"class":"well card"})
+
 anuncio.find('div', {"class":"value-card"})
-anuncio.find('p', {"class":"txt-value"}).get_text()
+anuncio.find('p', {"class":"txt-value"}).get_text() # pega o valor do carro
 card['value'] = anuncio.find('p', {"class":"txt-value"}).get_text() # colocando o valor dentro do card
-card
+# 
+# card.update['value'] = anuncio.find('p', {"class":"txt-value"}).get_text() # atualizando o dicionario
+#
+anuncio.find('div', {'class':'body-card'}).findAll('p')
+
+infos = anuncio.find('div', {'class':'body-card'}).findAll('p')
+#for info in infos:
+#    print(info)
+#for info in infos:
+#    print(info.get('class'), ' - ', info.get_text())
+#for info in infos:
+#    print(info.get('class')[0], ' - ', info.get_text()) 
+#for info in infos:
+#    print(info.get('class')[0].split('-')[-1], ' - ', info.get_text()) #[-1] menos a primeira palavra de cada linha
+for info in infos:
+    card[info.get('class')[0].split('-')[-1]] = info.get_text()
 
 # RESUMO
-card['value'] = anuncio.find('p', {"class":"txt-value"}).get_text()
+# card['value'] = anuncio.find('p', {"class":"txt-value"}).get_text()
+# infos = anuncio.find('div', {'class':'body-card'}).findAll('p')
+# for info in infos:
+#   card[info.get('class')[0].split('-')[-1]] = info.get_text()
